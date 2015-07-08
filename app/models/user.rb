9b7491e8f81be309045a3d79758e83e31a9457fa
rosaliejames <rej3kb@virginia.edu>
2015-07-08 12:40:49 +0200

    First commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	has_many :projects, through :participants
+	has_many :projects, through: :participants
 	has_many :conversations
 	has_many :groups
 end 
