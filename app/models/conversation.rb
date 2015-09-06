c0a7f37a413b9f6c520e360e2f5a67edb601a1fb
alan wei <aw3as@virginia.edu>
2015-09-06 11:52:54 -0400

    color for conversations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -6,11 +6,14 @@ class Conversation < ActiveRecord::Base
   after_create :assign_tag
 
   def assign_tag
-    tag = Faker::Number.hexadecimal(3)
-    while Conversation.find_by(:code => tag)
-      tag = Faker::Number.hexadecimal(3)
+    tags = %w(red green blue yellow purple teal white black orange pink grey maroon violet turquoise tan salmon plum orchid olive magenta lime ivory indigo gold fuchsia cyan azure lavender silver) - project.conversations.map(&:code)
+    if tags.size == 0
+      update(:code => '000')
+    elsif project.conversations.size == 1
+      update(:code => 'all')
+    else
+      update(:code => tags.sample)
     end
-    update(:code => tag)
   end
 
 end
