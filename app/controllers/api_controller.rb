cffaab31d52e4b0f5dd1f43544a307c7fdd56122
alan wei <aw3as@virginia.edu>
2015-09-06 09:59:19 -0400

    skip authenticate for api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,6 @@
 class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
 
   def receive
     puts params[:data][:text]
