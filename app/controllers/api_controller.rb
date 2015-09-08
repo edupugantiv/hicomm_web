9884b5ccf5568e026276237492546c7d1e75cd11
Alan Wei <awei@incuvateops.com>
2015-09-08 12:53:38 -0400

    better narrow down conversation

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -12,19 +12,26 @@ class ApiController < ApplicationController
     strings = body.split(' ')
     remaining_body = []
     strings.each do |string|
-      if string[0] == '@' or string[0] == '#'
-        if string[0] == '@'
-          project = Project.find_by(:code => string[1..-1])
-        elsif string[0] == '#'
-          conversation = Conversation.find_by(:code => string[1..-1])
-        end
+      if string[0] == '@' and Project.find_by(:code => string[1..-1])
+        project = string[1..-1]
+      elsif string[0] == '#' and Conversation.find_by(:code => string[1..-1])
+        conversation = string[1..-1]
       else
         remaining_body << string
       end
     end
-    logger.debug "sender: #{sender.id} project: #{project.id} conversation: #{conversation.id}"
+    if project and conversation
+      project = Project.find_by(:code => project)
+      if project
+        conversation = project.conversations.find_by(:code => conversation)
+      end
+    else
+      project, conversation = nil, nil
+    end
+
     if !sender or !project or !conversation or !project.conversations.include?(conversation)
       logger.debug "could not verify"
+      logger.debug "sender: #{sender.id} project: #{project.id} conversation: #{conversation.id}"
       render :nothing => true and return
     end
     Message.create(
