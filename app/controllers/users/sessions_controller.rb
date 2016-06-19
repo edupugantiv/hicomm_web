8c0adb6ca092588e113240a802d9d6340d768bbc
edupugantiv <vijayedupuganti@yahoo.com>
2016-06-19 14:07:24 -0700

    removed delete

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,5 @@
 module Users
 	class SessionsController < Devise::SessionsController
-	  before_action :check_user_status, only: [:create]
 
 		def new
 			redirect_to root_path
@@ -9,21 +8,6 @@ module Users
 		def auth_options
   		{ scope: resource_name, recall: "welcome#index" }
 		end
-
-  def check_user_status
-    user = User.find_by(mobile: params[:user][:mobile])
-    if !user.nil?
-      if !user.is_active
-        if current_user
-          sign_out current_user
-        end
-        redirect_to new_user_session_path, notice: 'Sorry for inconvenience, your account has been deactivated,
-                                                    please contact our support for further process.'
-      end
-    else
-      redirect_to new_user_session_path, notice: 'Invalid Mobile or Password'
-    end
-  end
 
 	end
 end	
